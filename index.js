4122960abfc12b5f2f5edbe220e7bcbd1c95ce69
David-Jacobis <david.jacobis@outlook.com>
2021-06-02 14:51:53 -0300

    Teste

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req,res)=> res.send("Funcionou!"))
-
-app.listen(3000);
+app.get('/', (req,res)=> res.send("Funcionou!"));
+app.listen(process.env.PORT || 3000);
